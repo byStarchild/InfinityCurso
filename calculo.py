c12b43ae2cec4bc552433471e68d8f3dfc673175
Rodrigo Augusto <96624818+byStarchild@users.noreply.github.com>
2024-06-15 11:19:57 -0300

    Add files via upload

---
+++ calculo.py
@@ -0,0 +1,9 @@
+num1 = int(input("Numero que indique a base: "))
+
+num2 = int(input("Numero que indique a altura: "))
+
+resultado = num1 * num2
+
+mensagem = f"O resultado da area é de {resultado}"
+
+print(mensagem)
